2ce9b3273127c4cc3da0ac6220c8d30bd7f1ce33
Aleksandr <werwebwer@mail.ru>
2020-10-14 01:49:36 +0300

    third

--- modules/task_1/smirnov_a_alternation_os_signs/alternation_os_signs.h
+++ modules/task_1/smirnov_a_alternation_os_signs/alternation_os_signs.h
@@ -1,13 +1,13 @@
-// Copyright 2018 Nesterov Alexander
-#ifndef MODULES_TEST_TASKS_TEST_MPI_OPS_MPI_H_
-#define MODULES_TEST_TASKS_TEST_MPI_OPS_MPI_H_
+// Copyright 2020 Smirnov Alexander
+#ifndef MODULES_TASK_1_SMIRNOV_A_ALTERNATION_OS_SIGNS_ALTERNATION_OS_SIGNS_H_
+#define MODULES_TASK_1_SMIRNOV_A_ALTERNATION_OS_SIGNS_ALTERNATION_OS_SIGNS_H_
 
 #include <vector>
 #include <string>
 
 std::vector<int> getRandomVector(int  sz);
 int getParallelOperations(std::vector<int> global_vec,
-	int count_size_vector);
+    int count_size_vector);
 int getSequentialOperations(std::vector<int> vec);
 
-#endif  // MODULES_TEST_TASKS_TEST_MPI_OPS_MPI_H_
+#endif  // MODULES_TASK_1_SMIRNOV_A_ALTERNATION_OS_SIGNS_ALTERNATION_OS_SIGNS_H_
--- modules/task_1/smirnov_a_alternation_os_signs/main.cpp
+++ modules/task_1/smirnov_a_alternation_os_signs/main.cpp
@@ -1,131 +1,129 @@
+// Copyright 2020 Smirnov Alexander
 #include <gtest-mpi-listener.hpp>
 #include <gtest/gtest.h>
 #include <vector>
 #include "./alternation_os_signs.h"
 
 TEST(Parallel_Operations_MPI, Test_0) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    std::vector<int> global_vec;
+    const int count_size_vector = 0;
 
-	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	std::vector<int> global_vec;
-	const int count_size_vector = 0;
+    if (rank == 0) {
+        global_vec = getRandomVector(count_size_vector);
+    }
 
-	if (rank == 0) {
-		global_vec = getRandomVector(count_size_vector);
-	}
+    int global_sum = getParallelOperations(global_vec, count_size_vector);
 
-	int global_sum = getParallelOperations(global_vec, count_size_vector);
-
-	if (rank == 0) {
-		int reference_sum = getSequentialOperations(global_vec);
-		ASSERT_EQ(reference_sum, global_sum);
-	}
+    if (rank == 0) {
+        int reference_sum = getSequentialOperations(global_vec);
+        ASSERT_EQ(reference_sum, global_sum);
+    }
 }
 TEST(Parallel_Operations_MPI, Test_1) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    std::vector<int> global_vec;
+    const int count_size_vector = 1;
 
-	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	std::vector<int> global_vec;
-	const int count_size_vector = 1;
+    if (rank == 0) {
+        global_vec = getRandomVector(count_size_vector);
+    }
 
-	if (rank == 0) {
-		global_vec = getRandomVector(count_size_vector);
-	}
+    int global_sum = getParallelOperations(global_vec, count_size_vector);
 
-	int global_sum = getParallelOperations(global_vec, count_size_vector);
-
-	if (rank == 0) {
-		int reference_sum = getSequentialOperations(global_vec);
-		ASSERT_EQ(reference_sum, global_sum);
-	}
+    if (rank == 0) {
+        int reference_sum = getSequentialOperations(global_vec);
+        ASSERT_EQ(reference_sum, global_sum);
+    }
 }
 TEST(Parallel_Operations_MPI, Test_10) {
-	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	std::vector<int> global_vec;
-	const int count_size_vector = 10;
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    std::vector<int> global_vec;
+    const int count_size_vector = 10;
 
-	if (rank == 0) {
-		global_vec = getRandomVector(count_size_vector);
-	}
+    if (rank == 0) {
+        global_vec = getRandomVector(count_size_vector);
+    }
 
-	int global_sum = getParallelOperations(global_vec, count_size_vector);
+    int global_sum = getParallelOperations(global_vec, count_size_vector);
 
-	if (rank == 0) {
-		int reference_sum = getSequentialOperations(global_vec);
-		ASSERT_EQ(reference_sum, global_sum);
-	}
+    if (rank == 0) {
+        int reference_sum = getSequentialOperations(global_vec);
+        ASSERT_EQ(reference_sum, global_sum);
+    }
 }
 TEST(Parallel_Operations_MPI, Test_100) {
-	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	std::vector<int> global_vec;
-	const int count_size_vector = 100;
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    std::vector<int> global_vec;
+    const int count_size_vector = 100;
 
-	if (rank == 0) {
-		global_vec = getRandomVector(count_size_vector);
-	}
+    if (rank == 0) {
+        global_vec = getRandomVector(count_size_vector);
+    }
 
-	int global_sum = getParallelOperations(global_vec, count_size_vector);
+    int global_sum = getParallelOperations(global_vec, count_size_vector);
 
-	if (rank == 0) {
-		int reference_sum = getSequentialOperations(global_vec);
-		ASSERT_EQ(reference_sum, global_sum);
-	}
+    if (rank == 0) {
+        int reference_sum = getSequentialOperations(global_vec);
+        ASSERT_EQ(reference_sum, global_sum);
+    }
 }
 TEST(Parallel_Operations_MPI, Test_5000) {
-	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	std::vector<int> global_vec;
-	const int count_size_vector = 5000;
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    std::vector<int> global_vec;
+    const int count_size_vector = 5000;
 
-	if (rank == 0) {
-		global_vec = getRandomVector(count_size_vector);
-	}
+    if (rank == 0) {
+        global_vec = getRandomVector(count_size_vector);
+    }
 
-	int global_sum = getParallelOperations(global_vec, count_size_vector);
+    int global_sum = getParallelOperations(global_vec, count_size_vector);
 
-	if (rank == 0) {
-		int reference_sum = getSequentialOperations(global_vec);
-		ASSERT_EQ(reference_sum, global_sum);
-	}
+    if (rank == 0) {
+        int reference_sum = getSequentialOperations(global_vec);
+        ASSERT_EQ(reference_sum, global_sum);
+    }
 }
 TEST(Parallel_Operations_MPI, Test_1000000) {
-	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	std::vector<int> global_vec;
-	const int count_size_vector = 1000000;
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    std::vector<int> global_vec;
+    const int count_size_vector = 1000000;
 
-	if (rank == 0) {
-		global_vec = getRandomVector(count_size_vector);
-	}
+    if (rank == 0) {
+        global_vec = getRandomVector(count_size_vector);
+    }
 
-	int global_sum = getParallelOperations(global_vec, count_size_vector);
+    int global_sum = getParallelOperations(global_vec, count_size_vector);
 
-	if (rank == 0) {
-		int reference_sum = getSequentialOperations(global_vec);
-		ASSERT_EQ(reference_sum, global_sum);
-	}
+    if (rank == 0) {
+        int reference_sum = getSequentialOperations(global_vec);
+        ASSERT_EQ(reference_sum, global_sum);
+    }
 }
 
 int main(int argc, char** argv) {
-	::testing::InitGoogleTest(&argc, argv);
-	MPI_Init(&argc, &argv);
-	int ProcNum, ProcRank;
-
-	MPI_Comm_size(MPI_COMM_WORLD, &ProcNum); 
-	MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
+    ::testing::InitGoogleTest(&argc, argv);
+    MPI_Init(&argc, &argv);
+    int ProcNum, ProcRank;
+    MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
+    MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
 
 #ifdef detail
-	if (ProcRank == 0) std::cout << ProcNum << std::endl;
+    if (ProcRank == 0) std::cout << ProcNum << std::endl;
 #endif
-	::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
-	::testing::TestEventListeners& listeners =
-		::testing::UnitTest::GetInstance()->listeners();
+    ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
+    ::testing::TestEventListeners& listeners =
+        ::testing::UnitTest::GetInstance()->listeners();
 
-	listeners.Release(listeners.default_result_printer());
-	listeners.Release(listeners.default_xml_generator());
+    listeners.Release(listeners.default_result_printer());
+    listeners.Release(listeners.default_xml_generator());
 
-	listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
-	return RUN_ALL_TESTS();
+    listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
+    return RUN_ALL_TESTS();
 }
