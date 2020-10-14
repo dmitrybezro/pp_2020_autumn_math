611bbd09cde743963b65ab450fe1ac4a266d3afd
BrandonRTL <43331560+BrandonRTL@users.noreply.github.com>
2020-10-15 00:01:26 +0300

    Stll cant

--- modules/task_1/kolesnikov_g_alphabetic_count/main.cpp
+++ modules/task_1/kolesnikov_g_alphabetic_count/main.cpp
@@ -11,96 +11,96 @@ TEST(Parallel_Operations_MPI, test_0) {
     const int size = 40;
 
     if (rank == 0) {
-		global_str = getRandomString(size);
+        global_str = getRandomString(size);
     }
 
     int global_counter = getParallelCount(global_str, size);
 
     if (rank == 0) {
-		int sequential_counter = getSequentialCount(global_str);
-		ASSERT_EQ(sequential_counter, global_counter);
-	}
+        int sequential_counter = getSequentialCount(global_str);
+        ASSERT_EQ(sequential_counter, global_counter);
+    }
 }
 TEST(Parallel_Operations_MPI, test_1) {
-	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	std::vector<char> global_str;
-	const int size = 100;
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    std::vector<char> global_str;
+    const int size = 100;
 
-	if (rank == 0) {
-		global_str = getRandomString(size);
-	}
+    if (rank == 0) {
+        global_str = getRandomString(size);
+    }
 
-	int global_counter = getParallelCount(global_str, size);
+    int global_counter = getParallelCount(global_str, size);
 
-	if (rank == 0) {
-		int sequential_counter = getSequentialCount(global_str);
-		ASSERT_EQ(sequential_counter, global_counter);
-	}
+    if (rank == 0) {
+        int sequential_counter = getSequentialCount(global_str);
+        ASSERT_EQ(sequential_counter, global_counter);
+    }
 }
 TEST(Parallel_Operations_MPI, test_2) {
-	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	std::vector<char> global_str;
-	const int size = 200;
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    std::vector<char> global_str;
+    const int size = 200;
 
-	if (rank == 0) {
-		global_str = getRandomString(size);
-	}
+    if (rank == 0) {
+        global_str = getRandomString(size);
+    }
 
-	int global_counter = getParallelCount(global_str, size);
+    int global_counter = getParallelCount(global_str, size);
 
-	if (rank == 0) {
-		int sequential_counter = getSequentialCount(global_str);
-		ASSERT_EQ(sequential_counter, global_counter);
-	}
+    if (rank == 0) {
+        int sequential_counter = getSequentialCount(global_str);
+        ASSERT_EQ(sequential_counter, global_counter);
+    }
 }
 TEST(Parallel_Operations_MPI, test_3) {
-	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	std::vector<char> global_str;
-	const int size = 400;
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    std::vector<char> global_str;
+    const int size = 400;
 
-	if (rank == 0) {
-		global_str = getRandomString(size);
-	}
+    if (rank == 0) {
+        global_str = getRandomString(size);
+    }
 
-	int global_counter = getParallelCount(global_str, size);
+    int global_counter = getParallelCount(global_str, size);
 
-	if (rank == 0) {
-		int sequential_counter = getSequentialCount(global_str);
-		ASSERT_EQ(sequential_counter, global_counter);
-	}
+    if (rank == 0) {
+        int sequential_counter = getSequentialCount(global_str);
+        ASSERT_EQ(sequential_counter, global_counter);
+    }
 }
 TEST(Parallel_Operations_MPI, test_4) {
-	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	std::vector<char> global_str;
-	const int size = 1000;
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    std::vector<char> global_str;
+    const int size = 1000;
 
-	if (rank == 0) {
-		global_str = getRandomString(size);
-	}
+    if (rank == 0) {
+        global_str = getRandomString(size);
+    }
 
-	int global_counter = getParallelCount(global_str, size);
+    int global_counter = getParallelCount(global_str, size);
 
-	if (rank == 0) {
-		int sequential_counter = getSequentialCount(global_str);
-		ASSERT_EQ(sequential_counter, global_counter);
-	}
+    if (rank == 0) {
+        int sequential_counter = getSequentialCount(global_str);
+        ASSERT_EQ(sequential_counter, global_counter);
+    }
 }
 
 int main(int argc, char** argv) {
-	::testing::InitGoogleTest(&argc, argv);
-	MPI_Init(&argc, &argv);
+    ::testing::InitGoogleTest(&argc, argv);
+    MPI_Init(&argc, &argv);
 
-	::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
-	::testing::TestEventListeners& listeners =
-		::testing::UnitTest::GetInstance()->listeners();
+    ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
+    ::testing::TestEventListeners& listeners =
+    ::testing::UnitTest::GetInstance()->listeners();
 
-	listeners.Release(listeners.default_result_printer());
-	listeners.Release(listeners.default_xml_generator());
+    listeners.Release(listeners.default_result_printer());
+    listeners.Release(listeners.default_xml_generator());
 
-	listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
-	return RUN_ALL_TESTS();
+    listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
+    return RUN_ALL_TESTS();
 }
