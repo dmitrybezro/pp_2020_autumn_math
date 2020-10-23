24ef48078a631d9c349a569663ea012b22c96f82
dashachernyh <dasha.chernyh.@mail.ru>
2020-10-23 22:12:20 +0300

    new_realiz

--- modules/task_1/chernyh_d_sum_of_values_ny_matrix_rows/main.cpp
+++ modules/task_1/chernyh_d_sum_of_values_ny_matrix_rows/main.cpp
@@ -5,95 +5,77 @@
 #include "./matrix_sum.h"
 
 TEST(Parallel_Operations_MPI, Test_Sum) {
-    int rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    std::vector<int> global_mat;
-    const int count_row = 5;
-	const int count_str = 5;
-
-    if (rank == 0) {
-        global_mat = getRandomMat(count_row,count_str);
-    }
-
-    std::vector<int> global_sum = getParallelOperations(global_mat, count_row,count_str);
-
-    if (rank == 0) {
-        std::vector<int> reference_sum = getSequentialOperations(global_mat,count_row,count_str);
-        ASSERT_EQ(reference_sum, global_sum);
-    }
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  std::vector<int> global_mat;
+  const int count_row = 5, count_str = 5;
+  if (rank == 0) {
+    global_mat = getRandomMat(count_row, count_str);
+  }
+  std::vector<int> global_sum = getParallelOperations(global_mat, count_row, count_str);
+  if (rank == 0) {
+    std::vector<int> reference_sum = getSequentialOperations(global_mat, count_row, count_str);
+    ASSERT_EQ(reference_sum, global_sum);
+  }
 }
+
 TEST(Parallel_Operations_MPI, Test_Sum_2) {
-	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	std::vector<int> global_mat;
-	const int count_row = 100;
-	const int count_str = 100;
-
-	if (rank == 0) {
-		global_mat = getRandomMat(count_row, count_str);
-	}
-
-	std::vector<int> global_sum = getParallelOperations(global_mat, count_row, count_str);
-
-	if (rank == 0) {
-		std::vector<int> reference_sum = getSequentialOperations(global_mat, count_row, count_str);
-		ASSERT_EQ(reference_sum, global_sum);
-	}
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  std::vector<int> global_mat;
+  const int count_row = 100, count_str = 100;
+  if (rank == 0) {
+    global_mat = getRandomMat(count_row, count_str);
+  }
+  std::vector<int> global_sum = getParallelOperations(global_mat, count_row, count_str);
+  if (rank == 0) {
+    std::vector<int> reference_sum = getSequentialOperations(global_mat, count_row, count_str);
+    ASSERT_EQ(reference_sum, global_sum);
 }
+}
+
 TEST(Parallel_Operations_MPI, Test_Sum_3) {
-	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	std::vector<int> global_mat;
-	const int count_row = 105;
-	const int count_str = 213;
-
-	if (rank == 0) {
-		global_mat = getRandomMat(count_row, count_str);
-	}
-
-	std::vector<int> global_sum = getParallelOperations(global_mat, count_row, count_str);
-
-	if (rank == 0) {
-		std::vector<int> reference_sum = getSequentialOperations(global_mat, count_row, count_str);
-		ASSERT_EQ(reference_sum, global_sum);
-	}
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  std::vector<int> global_mat;
+  const int count_row = 105, count_str = 213;
+  if (rank == 0) {
+    global_mat = getRandomMat(count_row, count_str); }
+  std::vector<int> global_sum = getParallelOperations(global_mat, count_row, count_str);
+  if (rank == 0) {
+    std::vector<int> reference_sum = getSequentialOperations(global_mat, count_row, count_str);
+    ASSERT_EQ(reference_sum, global_sum); }
 }
+
 TEST(Parallel_Operations_MPI, Test_Sum_4) {
-	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	std::vector<int> global_mat;
-	const int count_row = 1002;
-	const int count_str = 2001;
-
-	if (rank == 0) {
-		global_mat = getRandomMat(count_row, count_str);
-	}
-
-	std::vector<int> global_sum = getParallelOperations(global_mat, count_row, count_str);
-
-	if (rank == 0) {
-		std::vector<int> reference_sum = getSequentialOperations(global_mat, count_row, count_str);
-		ASSERT_EQ(reference_sum, global_sum);
-	}
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  std::vector<int> global_mat;
+  const int count_row = 1002, count_str = 2001;
+  if (rank == 0) {
+    global_mat = getRandomMat(count_row, count_str);
+  }
+  std::vector<int> global_sum = getParallelOperations(global_mat, count_row, count_str);
+  if (rank == 0) {
+    std::vector<int> reference_sum = getSequentialOperations(global_mat, count_row, count_str);
+    ASSERT_EQ(reference_sum, global_sum);
+  }
 }
+
 TEST(Parallel_Operations_MPI, Test_Sum_5) {
-	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	std::vector<int> global_mat;
-	const int count_row = 51;
-	const int count_str = 43;
-
-	if (rank == 0) {
-		global_mat = getRandomMat(count_row, count_str);
-	}
-
-	std::vector<int> global_sum = getParallelOperations(global_mat, count_row, count_str);
-
-	if (rank == 0) {
-		std::vector<int> reference_sum = getSequentialOperations(global_mat, count_row, count_str);
-		ASSERT_EQ(reference_sum, global_sum);
-	}
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  std::vector<int> global_mat;
+  const int count_row = 51, count_str = 43;
+  if (rank == 0) {
+    global_mat = getRandomMat(count_row, count_str); }
+  std::vector<int> global_sum = getParallelOperations(global_mat, count_row, count_str);
+  if (rank == 0) {
+    std::vector<int> reference_sum = getSequentialOperations(global_mat, count_row, count_str);
+    ASSERT_EQ(reference_sum, global_sum);
+  }
 }
+
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     MPI_Init(&argc, &argv);
