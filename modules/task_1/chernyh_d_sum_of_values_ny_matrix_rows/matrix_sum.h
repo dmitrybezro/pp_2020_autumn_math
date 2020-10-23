421cc4bd544ecf721ad8c40ca8d959aec4ebab84
dashachernyh <dasha.chernyh.@mail.ru>
2020-10-23 22:12:20 +0300

    new_realiz

--- modules/task_1/chernyh_d_sum_of_values_ny_matrix_rows/matrix_sum.h
+++ modules/task_1/chernyh_d_sum_of_values_ny_matrix_rows/matrix_sum.h
@@ -1,13 +1,13 @@
 // Copyright 2018 Nesterov Alexander
-#ifndef MODULES_TASK_1_NESTEROV_A_VECTOR_SUM_OPS_MPI_H_
-#define MODULES_TASK_1_NESTEROV_A_VECTOR_SUM_OPS_MPI_H_
+#ifndef MODULES_TASK_1_CHERNYH_D_SUM_OF_VALUES_NY_MATRIX_ROWS_MATRIX_SUM_H_
+#define MODULES_TASK_1_CHERNYH_D_SUM_OF_VALUES_NY_MATRIX_ROWS_MATRIX_SUM_H_
 
 #include <vector>
 #include <string>
 
 std::vector<int> getRandomMat(int  count_row, int count_str);
 std::vector<int> getParallelOperations(std::vector<int> global_mat,
-	int count_row, int count_str);
+  int count_row, int count_str);
 std::vector<int> getSequentialOperations(std::vector<int> mat, int count_row, int count_str);
 
-#endif  // MODULES_TASK_1_NESTEROV_A_VECTOR_SUM_OPS_MPI_H_
+#endif  // MODULES_TASK_1_CHERNYH_D_SUM_OF_VALUES_NY_MATRIX_ROWS_MATRIX_SUM_H_
