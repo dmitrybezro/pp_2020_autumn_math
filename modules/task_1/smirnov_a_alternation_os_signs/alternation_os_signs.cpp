f1112cdcc4ce268ce286f88d629be01261bb155a
Aleksandr <werwebwer@mail.ru>
2020-10-24 14:23:15 +0300

    fix

--- modules/task_1/smirnov_a_alternation_os_signs/alternation_os_signs.cpp
+++ modules/task_1/smirnov_a_alternation_os_signs/alternation_os_signs.cpp
@@ -1,22 +1,21 @@
-// Copyright 2018 Nesterov Alexander
-#include <mpi.h>
-#include <vector>
-#include <string>
-#include <random>
-#include <ctime>
-#include <algorithm>
-#include "../../../modules/task_1/smirnov_a_alternation_os_signs/alternation_os_signs.h"
+    // Copyright 2020 Smirnov Aleksandr
+    #include <mpi.h>
+    #include <vector>
+    #include <string>
+    #include <random>
+    #include <ctime>
+    #include <algorithm>
+    #include "./alternation_os_signs.h"
 
-
-std::vector<int> getRandomVector(int sz) {
+    std::vector<int> getRandomVector(int sz) {
     std::mt19937 gen;
     gen.seed(static_cast<unsigned int>(time(0)));
     std::vector<int> vec(sz);
     for (int i = 0; i < sz; i++) { vec[i] = gen() % 100 - 50; }
     return vec;
-}
+    }
 
-int getSequentialOperations(std::vector<int> vec) {
+    int getSequentialOperations(std::vector<int> vec) {
     const int  sz = vec.size();
     if (sz == 0) return 0;
     double t1, t2;
@@ -26,11 +25,11 @@ int getSequentialOperations(std::vector<int> vec) {
         if (vec[i] * vec[i + 1] < 0) change_of_sings++;
     }
     t2 = MPI_Wtime();
-    printf("time = %3.20f\n", t2 - t1);
+    printf("time seq= %3.20f\n", t2 - t1);
     return change_of_sings;
-}
+    }
 
-int getParallelOperations(std::vector<int> global_vec,
+    int getParallelOperations(std::vector<int> global_vec,
     int count_size_vector) {
     double t1, t2;
     int size, rank;
@@ -38,24 +37,14 @@ int getParallelOperations(std::vector<int> global_vec,
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     int change_of_sings = 0;
 
+    int* local_vec = new int[count_size_vector];
     if (count_size_vector == 0) return 0;
     if (rank == 0) {
+        for (int i = 0; i < count_size_vector; i++)
+            local_vec[i] = global_vec[i];
         t1 = MPI_Wtime();
-        for (int proc = 1; proc < size; proc++) {
-            MPI_Send(&global_vec[0], count_size_vector,
-                MPI_INT, proc, 0, MPI_COMM_WORLD);
-        }
     }
-
-    std::vector<int> local_vec(count_size_vector);
-    if (rank == 0) {
-        local_vec = std::vector<int>(global_vec.begin(),
-            global_vec.begin() + count_size_vector);
-    } else {
-        MPI_Status status;
-        MPI_Recv(&local_vec[0], count_size_vector, MPI_INT,
-            0, 0, MPI_COMM_WORLD, &status);
-    }
+    MPI_Bcast(local_vec, count_size_vector, MPI_INT, 0, MPI_COMM_WORLD);
 
     int local_count = 0;
     for (int i = rank; i < (count_size_vector - 1); i += size) {
@@ -66,7 +55,7 @@ int getParallelOperations(std::vector<int> global_vec,
 
     if (rank == 0) {
         t2 = MPI_Wtime();
-        printf("time = %3.20f\n", t2 - t1);
+        printf("time par= %3.20f\n", t2 - t1);
     }
     return change_of_sings;
-}
+    }
