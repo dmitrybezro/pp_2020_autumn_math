d96548f5bc87d128516a753981cc6091f4472f65
dmitrybezro <d.bezrodnow@gmail.com>
2020-10-30 17:10:30 +0300

    Third attempt

---
+++ modules/task_1/bezrodnov_d_min_elem_matrix/min_elem_matrix.h
@@ -0,0 +1,12 @@
+// Copyright 2020 Bezrodnov Dmitry
+#ifndef MODULES_TASK_1_BEZRODNOV_D_MIN_ELEM_MATRIX_MIN_ELEM_MATRIX_H_
+#define MODULES_TASK_1_BEZRODNOV_D_MIN_ELEM_MATRIX_MIN_ELEM_MATRIX_H_
+
+#include <vector>
+
+std::vector<int> getRandomMatrix(int row, int col);
+int getParallelOperations(std::vector<int> Matr, int row, int col);
+int getSequentialOperations(std::vector<int> Matr);
+
+#endif //  MODULES_TASK_1_BEZRODNOV_D_MIN_ELEM_MATRIX_MIN_ELEM_MATRIX_H_
+
